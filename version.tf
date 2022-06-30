af358b4510aed38b1e617e53c5686a2e52a0e2cd
Vinicius Yosiura <vinicius.yosiura@live.com>
2022-06-30 16:47:28 -0300

    corrigindo o version constraint do version.tf

--- version.tf
+++ version.tf
@@ -1,11 +1,13 @@
 terraform {
     required_providers {
-        source  = "mongodb/mongodbatas"
-        version = "~>1.3.1"
-    }
-    required_providers {
-        source  = "hashicorp/aws"
-        version = "~>4.20.1"
+        mongodbatlas = {
+            source  = "mongodb/mongodbatas"
+            version = "~>1.3.1"
+        }
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~>4.20.1"
+        }
     }
     required_version = ">=1.1.2,<1.3.0"
 }
